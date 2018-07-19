23a5dbcbefc0ba88c66502608196e55f7f0dc931
Myke Cameron <mykecameron@gmail.com>
2018-07-19 15:54:51 -0400

    add contain_meta matcher to match on subsets of meta

--- lib/jsonapi/rspec/meta.rb
+++ lib/jsonapi/rspec/meta.rb
@@ -7,6 +7,19 @@ module JSONAPI
             (!val || actual['meta'] == val)
         end
       end
+
+      ::RSpec::Matchers.define :contain_meta do |val|
+        match do |actual|
+          return false unless actual.key?('meta')
+          return true if actual['meta'] == val
+
+          if [actual['meta'], val].all?{|i| i.respond_to?(:to_a)}
+            (val.to_a - actual['meta'].to_a).empty?
+          else
+            false
+          end
+        end
+      end
     end
   end
 end
--- spec/meta_spec.rb
+++ spec/meta_spec.rb
@@ -25,3 +25,43 @@ RSpec.describe JSONAPI::RSpec, '#have_meta' do
     end
   end
 end
+
+RSpec.describe JSONAPI::RSpec, '#contain_meta' do
+  context 'when providing no value' do
+    it 'succeeds when meta is present' do
+      expect('meta' => {}).to contain_meta
+    end
+
+    it 'fails when meta is absent' do
+      expect({}).not_to contain_meta
+    end
+  end
+
+  context 'when providing a value' do
+    let(:actual) do
+      {
+        'meta' => {
+          foo: 'bar',
+          bar: 'baz',
+        }
+      }
+    end
+
+    it 'succeeds when actual meta is a superset of expected' do
+      expect(actual).to contain_meta(bar: 'baz')
+      expect(actual).to contain_meta(foo: 'bar')
+    end
+
+    it 'succeeds when actual meta exactly matches expected' do
+      expect(actual).to contain_meta(actual['meta'])
+    end
+
+    it 'fails when meta mismatches' do
+      expect(actual).not_to have_meta(beyond_foo: 'bar')
+    end
+
+    it 'fails when meta is absent' do
+      expect({}).not_to have_meta(foo: 'bar')
+    end
+  end
+end
